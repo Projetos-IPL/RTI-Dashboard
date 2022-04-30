7362d9cdcdb7f25ec18cb2c95c0cc17b089d5ad3
Afonso Santos <afonso.faisca1@gmail.com>
2022-04-30 22:41:47 +0100

    Login

--- src/components/forms/LoginForm/LoginForm.js
+++ src/components/forms/LoginForm/LoginForm.js
@@ -1,28 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function LoginForm() {
-    return (
-        <form>
-            <i className="fa-solid fa-building-shield login-logo"></i>
-            <h1 className="h3 my-4 fw-normal">Sistema de Segurança</h1>
+function LoginForm({ onLoginFormSubmit, onLoginResult }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-            <div id="error-banner" role="alert"></div>
+  const onChangeUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-            <div className="form-floating mb-2">
-                <input className="form-control" id="username-input" placeholder="Utilizador" type="text"/>
-                <label htmlFor="username-input">Utilizador</label>
-            </div>
-            <div className="form-floating">
-                <input className="form-control" id="password-input" placeholder="Palavra-Passe" type="password"/>
-                <label htmlFor="password-input">Palavra-Passe</label>
-            </div>
-            <button className="w-100 btn btn-lg btn-dark mt-4" id="submit-input" type="submit">
-                {"Entrar"}
-                <i className="fa-solid fa-spinner fa-spin-pulse" id="loading-spinner"></i>
-            </button>
-            <p className="mt-5 mb-3 text-muted">&copy; 2022 - Afonso Santos, Iúri Raimundo</p>
-        </form>
-    );
+  const onChangePassowrdChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+
+    const loginData = {
+      username,
+      password,
+    };
+
+    onLoginFormSubmit(loginData);
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <i className="fa-solid fa-building-shield login-logo"></i>
+      <h1 className="h3 my-4 fw-normal">Sistema de Segurança</h1>
+
+      {onLoginResult && (
+        <div id="error-banner" role="alert">
+          {onLoginResult.message}
+        </div>
+      )}
+
+      <div className="form-floating mb-2">
+        <input
+          className="form-control"
+          id="username-input"
+          placeholder="Utilizador"
+          type="text"
+          value={username}
+          onChange={onChangeUsernameChange}
+        />
+        <label htmlFor="username-input">Utilizador</label>
+      </div>
+      <div className="form-floating">
+        <input
+          className="form-control"
+          id="password-input"
+          placeholder="Palavra-Passe"
+          type="password"
+          value={password}
+          onChange={onChangePassowrdChange}
+        />
+        <label htmlFor="password-input">Palavra-Passe</label>
+      </div>
+      <button
+        className="w-100 btn btn-lg btn-dark mt-4"
+        id="submit-input"
+        type="submit"
+      >
+        {"Entrar"}
+        <i
+          className="fa-solid fa-spinner fa-spin-pulse"
+          id="loading-spinner"
+        ></i>
+      </button>
+      <p className="mt-5 mb-3 text-muted">
+        &copy; 2022 - Afonso Santos, Iúri Raimundo
+      </p>
+    </form>
+  );
 }
 
 export default LoginForm;
--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LoginForm from "../../components/forms/LoginForm/LoginForm";
 import "./LoginScreen.css";
+import request from "../../utils/utils.js";
 
 function LoginScreen() {
+  const [loginResult, setLoginResult] = useState({});
+
+  useEffect(() => {
+    console.log(loginResult);
+  }, [loginResult]);
+
+  const formSubmitDataHandler = (formData) => {
+    console.log(formData);
+
+    request("POST", "auth.php", formData, null)
+      .then((res) => {
+        setLoginResult({
+          message: "Login com sucesso",
+          type: "success",
+        });
+
+        console.log(loginResult);
+      })
+      .catch((err) => {
+        setLoginResult({
+          message: err.message,
+          type: "danger",
+        });
+      });
+  };
+
   return (
     <main className="form-signin">
-      <LoginForm />
+      <LoginForm
+        onLoginFormSubmit={formSubmitDataHandler}
+        onLoginResult={loginResult}
+      />
     </main>
   );
 }
